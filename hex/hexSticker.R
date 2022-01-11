89b992a323ffc511d83f9d9019b709c3f2b87407
rl627 <rl627@drexel.edu>
2022-01-11 11:39:22 -0500

    hex_logo exported to /man

--- hex/hexSticker.R
+++ hex/hexSticker.R
@@ -4,7 +4,7 @@ sticker("DragonIcon_Blue_HEX.png", package="shinyUHC",
         p_color  = "#303C51", p_family = "lato",p_size=25,
         h_fill="#ffffff", h_color="#303C51",
         s_x=1, s_y=.75, s_width=.6,
-        filename="imgfile.png")
+        filename="../man/figures/hex_logo.png")
 
 
 
