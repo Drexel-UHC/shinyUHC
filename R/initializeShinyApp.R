38e517e4ccad2ba4ba9efcbe15acb0baf5e069a5
rl627 <rl627@drexel.edu>
2022-04-12 16:30:04 -0400

    generalized initializeShinyApp() for UHC themed templates #1

--- R/initializeShinyApp.R
+++ R/initializeShinyApp.R
@@ -19,11 +19,16 @@ initializeShinyApp = function(dest = './', theme = "SALURBAL"){
 
 
   ## 2. Get endpoints
-  endpoints = c("app.R", "CSS/Header.css", "CSS/Home.css", "CSS/NavbarPage.css",
-                "HTML/FontAwesomeLoader.html", "HTML/Footer.html", "HTML/Header.html",
-                "HTML/Home.html", "R/Modules/OldFaithful.R", "www/favicon.ico",
-                "www/LAC_logo.png", "www/SALURBAL_logo1.jpg", "www/UHC_logo.png")
+  endpoints_SALURBAL = list(c("app.R", "CSS/Header.css", "CSS/Home.css", "CSS/NavbarPage.css",
+                              "HTML/FontAwesomeLoader.html", "HTML/Footer.html", "HTML/Header.html",
+                              "HTML/Home.html", "R/Modules/OldFaithful.R", "www/favicon.ico",
+                              "www/LAC_logo.png", "www/SALURBAL_logo1.jpg", "www/UHC_logo.png"))
+  endpoints_UHC = list(c("app.R", "CSS/Header.css", "CSS/Home.css", "CSS/NavbarPage.css",
+                    "HTML/FontAwesomeLoader.html", "HTML/Footer.html", "HTML/Header.html",
+                    "HTML/Home.html", "R/Modules/OldFaithful.R",
+                    "www/favicon.ico","www/logo.png"))
 
+  endpoints = unlist(ifelse(theme=="SALURBAL",endpoints_SALURBAL,endpoints_UHC))
 
   ## 3. Download template
   baseURL=paste0("https://raw.githubusercontent.com/Drexel-UHC/shinyUHC/main/Templates/",theme,"/")
