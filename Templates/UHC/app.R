45be568cf1217336fcaa9f2283a01222be885190
rl627 <rl627@drexel.edu>
2022-05-10 18:04:18 -0400

    Added On Load feature to UHC template #4

--- Templates/UHC/app.R
+++ Templates/UHC/app.R
@@ -1,12 +1,28 @@
 {
   ## Dependencies
   library(shiny)
+  library(waiter)
 
   ## Load Modules
   source("R/Modules/OldFaithful.R")
+
+  ## Loader code: Global Scope
+  loading_screen = div(
+    tags$img(
+      src = "logo.png",
+      height = 175
+    ),
+    div(
+      style = "padding-top: 50px;",
+      spin_loader()) )
 }
 
 ui <- fluidPage(
+  ## Loader code: UI (start)
+  useWaiter(),
+  waiterShowOnLoad(html = loading_screen,
+                   color = 'white'),
+  ## Loader code: UI (end)
   tags$head(includeCSS("CSS/Header.css")),
   tags$head(includeCSS("CSS/NavbarPage.css")),
   tags$head(includeCSS("CSS/Home.css")),
@@ -21,6 +37,12 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
+
+  ## Loader code: Server (start)
+  Sys.sleep(5) # do something that takes time
+  waiter_hide()
+  ## Loader code: Server (end)
+
   OldFaithful_Server("Figure1")
 }
 
