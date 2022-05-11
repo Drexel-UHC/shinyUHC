0b68d6fc8e453f16de5c51ea2e2cfa4b93a942ab
rl627 <rl627@drexel.edu>
2022-05-11 10:05:20 -0400

    initializeShinyApp: checks for existing directories + enabled nested directory creation

--- R/initializeShinyApp.R
+++ R/initializeShinyApp.R
@@ -1,7 +1,7 @@
 #' Initialize a themed shiny app
 #'
-#' @param dest the path to initialize your app; you should not sepcify the file name, just the directory.
-#' './folder/app.R' is wrong. './folder/' is correct.  By default it is the current directory.
+#' @param dest the path to initialize your app. A few rules: 1) you should not sepcify the file name, just the directory.
+#' './folder/app.R' is wrong. './folder/' is correct. 2) right  now only creation of folder within current directory.
 #' @param theme which theme do you want: Two options are available "UHC" or "SALURBAL"
 #'
 #' @return A character vector.
@@ -11,37 +11,48 @@
 #' initializeShinyApp(dest = "./", theme = "SALURBAL")
 
 initializeShinyApp = function(dest = './', theme = "SALURBAL"){
-  # dest = "dest";theme = "SALURBAL"
+  # theme = "SALURBAL"
+  # dest = ""
+  # dest = "dest";
+  # dest = 'app'
+  # dest = './app/'
+  # dest = 'apps/app'
 
-  ## 1. Setup folders
-  dest = ifelse(stringr::str_sub(dest,-1L)=='/',dest,paste0(dest,'/'))
-  if(!dest%in%c("./",'/',"")){dir.create(dest)}
-  purrr::map(c("CSS","HTML","www","R","R/Modules"),~dir.create(paste0(dest,.x),showWarnings=F))
+  ## 0. Format directories
+  dest_search = ifelse( stringr::str_sub(dest,1,1)=='.',stringr::str_sub(dest,3,-1L),dest)
+  dest_search = ifelse(stringr::str_sub(dest_search,-1L)=='/',stringr::str_sub(dest_search,1,-1L-1),dest_search)
+  dest_output = paste0(dest_search,"/")
+
+  ## 1. Check if output exists. If it does then throw an error
+  if (file.exists(dest_search)) stop('Output directory already exists, please choose a new directory.')
+  if (dest_search==""&file.exists('app.R'))  stop('Output directory already exists, please choose a new directory.')
+
+  ## 2. Setup folders
+  if (!dest%in%c("./",'/',"")) {dir.create(dest_search, recursive = TRUE)}
+  purrr::map(c("CSS","HTML","www","R","R/Modules"),~dir.create(paste0(dest_output,.x), recursive = TRUE,showWarnings=T))
 
 
-  ## 2. Get endpoints
+  ## 3. Get endpoints
   endpoints_SALURBAL = list(c("app.R", "CSS/Header.css", "CSS/Home.css", "CSS/NavbarPage.css",
                               "HTML/FontAwesomeLoader.html", "HTML/Footer.html", "HTML/Header.html",
                               "HTML/Home.html", "R/Modules/OldFaithful.R", "www/favicon.ico",
                               "www/LAC_logo.png", "www/SALURBAL_logo1.jpg", "www/UHC_logo.png"))
   endpoints_UHC = list(c("app.R", "CSS/Header.css", "CSS/Home.css", "CSS/NavbarPage.css",
-                    "HTML/FontAwesomeLoader.html", "HTML/Footer.html", "HTML/Header.html",
-                    "HTML/Home.html", "R/Modules/OldFaithful.R",
-                    "www/favicon.ico","www/logo.png"))
+                         "HTML/FontAwesomeLoader.html", "HTML/Footer.html", "HTML/Header.html",
+                         "HTML/Home.html", "R/Modules/OldFaithful.R",
+                         "www/favicon.ico","www/logo.png"))
 
   endpoints = unlist(ifelse(theme=="SALURBAL",endpoints_SALURBAL,endpoints_UHC))
 
-  ## 3. Download template
+  ## 4. Download template
   baseURL=paste0("https://raw.githubusercontent.com/Drexel-UHC/shinyUHC/main/Templates/",theme,"/")
   purrr::walk(endpoints,~{
     download.file(url = paste0(baseURL,.x),
-                  destfile = paste0(dest,.x), mode = 'wb')
+                  destfile = paste0(dest_output,.x), mode = 'wb')
     print(paste0("Import: ",.x))
   })
 
-  ## 4. Open new app file
-  cleaned_dest = ifelse(stringr::str_sub(dest,1,1)=='.',stringr::str_sub(dest,2,-1L),dest)
-  cleaned_dest = ifelse(stringr::str_sub(dest,-1L)!='/',paste0(dest,'/'),dest)
-  print(paste0("* ShinyUHC app successfully initialized at ",paste0(getwd(),cleaned_dest,'app.R')))
-  if(rstudioapi::isAvailable()){rstudioapi::navigateToFile(paste0(cleaned_dest,'app.R'))}
+  ## 5. Open new app file
+  print(paste0("* ShinyUHC app successfully initialized at ",paste0(getwd(),'/',dest_output,'app.R')))
+  if(rstudioapi::isAvailable()){rstudioapi::navigateToFile(paste0(dest_output,'app.R'))}
 }
