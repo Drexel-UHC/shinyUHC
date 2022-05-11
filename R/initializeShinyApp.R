2e78f92d5b161f4c1940655557834e163cf6474d
rl627 <rl627@drexel.edu>
2022-05-11 10:19:04 -0400

    initializeShinyApp: fixed "" directory bug + minimized function messages

--- R/initializeShinyApp.R
+++ R/initializeShinyApp.R
@@ -21,7 +21,7 @@ initializeShinyApp = function(dest = './', theme = "SALURBAL"){
   ## 0. Format directories
   dest_search = ifelse( stringr::str_sub(dest,1,1)=='.',stringr::str_sub(dest,3,-1L),dest)
   dest_search = ifelse(stringr::str_sub(dest_search,-1L)=='/',stringr::str_sub(dest_search,1,-1L-1),dest_search)
-  dest_output = paste0(dest_search,"/")
+  dest_output = ifelse(dest_search=="","",paste0(dest_search,"/"))
 
   ## 1. Check if output exists. If it does then throw an error
   if (file.exists(dest_search)) stop('Output directory already exists, please choose a new directory.')
@@ -29,7 +29,7 @@ initializeShinyApp = function(dest = './', theme = "SALURBAL"){
 
   ## 2. Setup folders
   if (!dest%in%c("./",'/',"")) {dir.create(dest_search, recursive = TRUE)}
-  purrr::map(c("CSS","HTML","www","R","R/Modules"),~dir.create(paste0(dest_output,.x), recursive = TRUE,showWarnings=T))
+  purrr::map(c("CSS","HTML","www","R","R/Modules"),~dir.create(paste0(dest_output,.x), recursive = TRUE,showWarnings=F))
 
 
   ## 3. Get endpoints
@@ -48,11 +48,14 @@ initializeShinyApp = function(dest = './', theme = "SALURBAL"){
   baseURL=paste0("https://raw.githubusercontent.com/Drexel-UHC/shinyUHC/main/Templates/",theme,"/")
   purrr::walk(endpoints,~{
     download.file(url = paste0(baseURL,.x),
-                  destfile = paste0(dest_output,.x), mode = 'wb')
-    print(paste0("Import: ",.x))
+                  destfile = paste0(dest_output,.x),
+                  mode = 'wb',
+                  quiet = T)
+
   })
 
   ## 5. Open new app file
-  print(paste0("* ShinyUHC app successfully initialized at ",paste0(getwd(),'/',dest_output,'app.R')))
+  message(paste0("App initialized at ",paste0(getwd(),'/',dest_output,'app.R')))
+  message("New app.R has been opened automatically.")
   if(rstudioapi::isAvailable()){rstudioapi::navigateToFile(paste0(dest_output,'app.R'))}
 }
